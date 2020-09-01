a197aacc430714c53952b1c0e8b7bfbb1a7c5ca8
Josoroma <pablo@josoroma.com>
2020-09-01 08:06:02 -0600

    Challenge-4: add - react.lazy and suspense approach

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import CssBaseline from "@material-ui/core/CssBaseline";
 
 import { SearchValueProvider } from "context/SearchValue";
 
-import { Counties, States, Toolbar } from "components";
+import { ContentMessage, Toolbar } from "components";
+
+// Route Components - App Screens/Pages
+const States = React.lazy(() => import("components/States/States"));
+const Counties = React.lazy(() => import("components/Counties/Counties"));
 
 const App = () => (
   <div>
     <CssBaseline />
     <SearchValueProvider>
-      <Router>
-        <div>
+      <Suspense
+        fallback={
+          <div data-testid="id-app-load-page-progress">
+            <ContentMessage type="progress" />
+          </div>
+        }
+      >
+        <Router>
           <Toolbar />
           <Switch>
             <Route path="/:stateId/counties">
@@ -21,8 +31,8 @@ const App = () => (
               <States />
             </Route>
           </Switch>
-        </div>
-      </Router>
+        </Router>
+      </Suspense>
     </SearchValueProvider>
   </div>
 );
